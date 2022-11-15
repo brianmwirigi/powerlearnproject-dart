3f3a5d716b4c8e57f131c5acecb6dbda77e621ad
mwirigi <bmmaingi17@gmail.com>
2022-11-15 21:07:17 +0300

    running

--- datatypes.dart
+++ datatypes.dart
@@ -4,11 +4,11 @@ void main()
     String myName = "Brian Mwirigi"; // string which is series of text
     int myAGe = 24;// INTEGER which is a whole number 
     double myWeight = 100.5; //a number with a decimal point
-    List <double> grades = [60.9, 71.5, 50.5]//list or array [60.9, 71.5, 50.5]
-    List <dynamic> income = ['Brian', 71.5, 50.5] //dynamic data
+    List <double> grades = [60.9, 71.5, 50.5];//list or array [60.9, 71.5, 50.5]
+    List <dynamic> income = ['Brian', 71.5, 50.5]; //dynamic data
     bool isRaining = true;//boolean which is a true or false statement
     print('\u2667');// runes are string that display special characters
-    Map myInformation = { 'name' : "brian mwirigi", 'age' : 200, 'net_worth' : 10,000};  //map - dictionary stores information ink key value pair
+    Map myInformation = { 'name' : "brian mwirigi", 'age' : 200, 'net_worth' : 10000};  //map - dictionary stores information ink key value pair
 
     //print is a method methodname()
 }
