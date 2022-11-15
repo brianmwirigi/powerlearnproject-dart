6eddf607ee1d4a29b5400d5fa0766fc0aaf892c5
mwirigi <bmmaingi17@gmail.com>
2022-11-15 19:52:24 +0300

    string

--- datatypes.dart
+++ datatypes.dart
@@ -6,9 +6,10 @@ void main()
     int myAGe = 24;// INTEGER which is a whole number 
     double myWeight = 100.5; //a number with a decimal point
     List <double> grades = [60.9, 71.5, 50.5]//list or array [60.9, 71.5, 50.5]
+    List <dynamic> income = ['Brian', 71.5, 50.5] //dynamic data
     bool isRaining = true;//boolean which is a true or false statement
     print('\u2667');// runes are string that display special characters
-    //map - dictionary
+    Map myInformation = { 'name' : "brian mwirigi", 'age' : 200, 'net_worth' : 10,000};  //map - dictionary stores information ink key value pair
 
-
+    //print is a method methodname()
 }
---
+++ string.dart
@@ -0,0 +1,12 @@
+void main()
+{
+//string - concantenating string
+
+String firstName = "Brian";
+String secondName = "Mwirigi";
+
+print(firstName + ' ' + secondName);
+print("$firstName $secondName"); //modern
+print([1]);//indexing in string
+
+}
