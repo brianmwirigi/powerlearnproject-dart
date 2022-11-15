8b84084469da4fd99fa1a8457d8919e78b4af9f9
mwirigi <bmmaingi17@gmail.com>
2022-11-15 19:25:46 +0300

    dart programming

---
+++ datatypes.dart
@@ -0,0 +1,14 @@
+Run | Debug
+void main()
+{
+    //data types
+    String myName = "Brian Mwirigi"; // string which is series of text
+    int myAGe = 24;// INTEGER which is a whole number 
+    double myWeight = 100.5; //a number with a decimal point
+    List <double> grades = [60.9, 71.5, 50.5]//list or array [60.9, 71.5, 50.5]
+    bool isRaining = true;//boolean which is a true or false statement
+    print('\u2667');// runes are string that display special characters
+    //map - dictionary
+
+
+}
