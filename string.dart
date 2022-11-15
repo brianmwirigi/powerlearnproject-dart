a3e1b5b6f4d770b0090cbfa30c993d92b7876d53
mwirigi <bmmaingi17@gmail.com>
2022-11-15 20:03:56 +0300

    string regular expressions

--- string.dart
+++ string.dart
@@ -11,4 +11,6 @@ print([1]);//indexing in string
 
 print(firstName.toLowerCase());
 print(firstName.contains('Brian')); //string methods that checks
+print(firstName.split(' ')); //created a list
+//regular expressions
 }
