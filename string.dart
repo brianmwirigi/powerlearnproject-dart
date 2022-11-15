55b95079ea0b527383c2b5920c59b835331b4380
mwirigi <bmmaingi17@gmail.com>
2022-11-15 21:23:29 +0300

    dart running

--- string.dart
+++ string.dart
@@ -1,16 +1,16 @@
-void main()
-{
+void main() {
 //string - concantenating string
 
-String firstName = "Brian";
-String secondName = "Mwirigi";
+  String firstName = "Brian";
+  String secondName = "Mwirigi";
 
-print(firstName + ' ' + secondName);
-print("$firstName $secondName"); //modern
-print([1]);//indexing in string
+  print(firstName + ' ' + secondName);
+  print("$firstName $secondName"); //modern
+  print([1]); //indexing in string
 
-print(firstName.toLowerCase());
-print(firstName.toLowerCase.contains('Brian')); //string methods that checks for name and chaining of commands
-print(firstName.split(' ')); //created a list
+  print(firstName.toLowerCase());
+  print(firstName.toLowerCase().contains(
+      'brian')); //string methods that checks for name and chaining of commands
+  print(firstName.split(' ')); //created a list
 //regular expressions
 }
